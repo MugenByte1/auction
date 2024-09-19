21b996b78e0fffc1ac60c99bad290e7028121717
amirreza_mehrali <13ar79mp@gmail.com>
2024-09-19 12:52:37 +0330

    .

--- customer/views.py
+++ customer/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from .serializers import *
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import *
-
+from provider.models import *
+from provider.Serializers import *
+from django.http.response import HttpResponse
+from datetime import timedelta
 
 class ListCreateOffer(ListCreateAPIView):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
 
     def perform_create(self, serializer):
-        if .auction.price >= self.price
+        auction = Auction.objects.get(id = serializer.validated_data['auction'])
+        price = serializer.validated_data['price']
+        if auction.price >= price:
+            raise ValueError('price invalid')
+        else:
+            auction.start_time += timedelta(minutes=10)
+            auction.save()
+            return super().perform_create(serializer)
